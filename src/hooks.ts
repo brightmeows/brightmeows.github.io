55ff1463dedf12f56393cd43b1c72228b9134584
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-01-19 06:37:18 +0800

    feat: extract bms table page (#417)
    * feat: extract bms table page
    
    * fix: lint

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,3 +1,4 @@
+import type { Handle } from "@sveltejs/kit";
 import { deLocalizeUrl } from "./lib/paraglide/runtime.js";
 
 export const reroute = ({ url }: { url: URL }) => deLocalizeUrl(url).pathname;
@@ -8,7 +9,7 @@ export const transport = {};
  * 客户端 hooks
  * 在开发模式下执行
  */
-export const handle = async ({ event, resolve }: any) => {
+export const handle: Handle = async ({ event, resolve }) => {
   const response = await resolve(event);
 
   const contentType = response.headers.get("content-type");
@@ -19,7 +20,8 @@ export const handle = async ({ event, resolve }: any) => {
   // 检查是否设置了 bmstableMeta
   if (event.locals.bmstableMeta) {
     const html = await response.text();
-    const bmstableMeta = `<meta name="bmstable" content="${event.locals.bmstableMeta}" />`;
+    const bmstableMeta =
+      `<meta name="bmstable" content="${event.locals.bmstableMeta}" />`;
     return new Response(html.replace("%bmstable.meta%", bmstableMeta), {
       headers: response.headers,
     });
--- src/routes/bms/table/self-sp/+page.server.ts
+++ src/routes/bms/table/self-sp/+page.server.ts
@@ -4,7 +4,7 @@ import type { PageServerLoad } from "./$types";
  * 服务端加载数据
  * 设置 locals，告诉 hooks 使用相对路径注入 bmstable meta 标签
  */
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = ({ locals }) => {
   locals.bmstableMeta = "./header.json";
 
   return {};
