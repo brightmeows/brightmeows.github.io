e21ed33191365df4483fadd03d16f7be86456691
BrightMeowS <brightmeows@163.com>
2026-06-01 10:48:53 +0800

    refactor: remove /bms/table/mirror-proxy and /bms/table-mirror routes
    - Delete mirror-proxy route (4 files) and static data (2 files)
    - Delete table-mirror redirect route (depends on mirror-proxy)
    - Remove mirror-proxy deployment steps from CI

--- src/routes/bms/table-mirror/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-
-export const prerender = true;
-
-export const load = () => {
-  // eslint-disable-next-line @typescript-eslint/only-throw-error
-  throw redirect(308, "/bms/table/mirror");
-};
--- src/routes/bms/table/mirror-proxy/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from "./$types";
-
-import { formatTitle } from "$lib/utils/title";
-
-export const load: PageServerLoad = () => {
-  return {
-    title: formatTitle("BMS 难度表镜像"),
-  };
-};
